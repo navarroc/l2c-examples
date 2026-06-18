6cef43bc8628dbc07423eeea83dc53a73d58bd71
Chris Navarro <cmnavarr@illinois.edu>
2026-06-18 14:48:28 -0500

    Updated Snakemake script to generate config.yaml

--- scripts/snakemake/run_snakemake.sh
+++ scripts/snakemake/run_snakemake.sh
@@ -23,37 +23,74 @@ echo "Activate Snakemake"
 source ~/snakemake/myenv/bin/activate
 
 # TODO the singularity args could be made configurable
-snakemake --profile "$PROFILE" --singularity-args "--cwd /app --bind $(pwd)/output:/app/output" --jobs 1
+snakemake --profile "$PROFILE" --jobs 1 {{ IMAGE_CLI }}
 
 EOF
 }
 
-ACCOUNT_NAME="ncsa-ic"
-PARTITION="IllinoisComputes"
+config_template() {
+cat <<'EOF'
+executor: slurm
+use-singularity: true
+latency-wait: 60
+
+default-resources:
+  slurm_partition: "{{ PARTITION }}"
+  runtime: 5
+  mem_mb: 4000
+  cpus_per_task: 1
+  slurm_account: "{{ ACCOUNT }}"
+  nodes: 1
+  {{ GPU_OPTION }}
+EOF
+}
+
+JOB_ACCOUNT=""
+JOB_PARTITION=""
+WORKFLOW_ACCOUNT=""
+WORKFLOW_PARTITION=""
+
 HELP="NO"
 USE_SLURM="NO"
 # By default, don't remove the temporary files - users might want to see this to write their own
 CLEANUP="NO"
+NUM_GPUS=0
 
-while getopts "a:chp:s" opt; do
+while getopts "a:cg:hi:p:sw:" opt; do
     case $opt in
         a)
-            ACCOUNT_NAME="$OPTARG"
+            JOB_ACCOUNT="$OPTARG"
         ;;
         c)
             CLEANUP="YES"
         ;;
+        g)
+            NUM_GPUS="$OPTARG"
+        ;;
         h)
             HELP="YES"
         ;;
+        i)
+            WORKFLOW_PARTITION="$OPTARG"
+        ;;
         p)
-            PARTITION="$OPTARG"
+            JOB_PARTITION="$OPTARG"
         ;;
         s)
             USE_SLURM="YES"
         ;;
+        w)
+            WORKFLOW_ACCOUNT="$OPTARG"
+        ;;
     esac
 done
+
+# For now, we assume all other arguments are for the container
+shift $((OPTIND - 1))
+IMAGE_CLI="$@"
+
+echo "These are the arguments we'll pass to the workflow steps"
+echo $IMAGE_CLI
 
 if [ "$HELP" == "YES" ]; then
     echo "Usage : $0 <-a Account Name > <-p Partition> [-h]"
@@ -65,7 +102,15 @@ if [ "$HELP" == "YES" ]; then
     exit 0
 fi
 
-echo "Job will run with account $ACCOUNT_NAME and in partition $PARTITION"
+if [[ -z "$WORKFLOW_ACCOUNT" ]]; then
+  WORKFLOW_ACCOUNT=$JOB_ACCOUNT
+fi
+
+if [[ -z "$WORKFLOW_PARTITION" ]]; then
+  WORKFLOW_PARTITION=$JOB_PARTITION
+fi
+
+echo "Job will run with account $JOB_ACCOUNT and in partition $JOB_PARTITION"
 
 # TODO add local profile execution option for running on a users laptop
 
@@ -75,15 +120,15 @@ hostname="${HOSTNAME%%.*}"
 if [[ "$hostname" == dt-login* ]]; then
     echo "Running on Delta"
     PYTHON="cray-python/3.11.7"
-    PROFILE="profiles/delta"
+#    PROFILE="profiles/delta"
 elif [[ "$hostname" == gh-login* ]]; then
     echo "Running on Delta AI"
     PYTHON="python/3.11.9"
-    PROFILE="profiles/delta-ai"
+#    PROFILE="profiles/delta-ai"
 elif [[ "$hostname" == cc-login* ]]; then
     echo "Running on campus cluster"
     PYTHON="python/3.11.11"
-    PROFILE="profiles/ccluster"
+#    PROFILE="profiles/ccluster"
 else
     echo "Unknown host - it needs to be added to use this script: $hostname"
     exit 1
@@ -103,22 +148,38 @@ else
     pip install snakemake snakemake-executor-plugin-slurm
 fi
 
+
+echo "Creating Snakemake config"
+# Check if using GPUS
+GPU_OPTION=""
+if [[ $NUM_GPUS -gt 0 ]]; then
+  GPU_OPTION="slurm_extra: \"--gres=gpu:${NUM_GPUS}\""
+fi
+
+PROFILE=config.yaml
+config_template > config.yaml
+sed  -i -e "s|{{ PARTITION }}|$JOB_PARTITION|g" \
+     -i -e "s|{{ ACCOUNT }}|$JOB_ACCOUNT|g" \
+     -i -e "s|{{ GPU_OPTION }}|$GPU_OPTION|g" config.yaml
+
 if [ "$USE_SLURM" == "YES" ]; then
     sbatch_template > run_job.sh
-    sed -i -e "s|{{ PARTITION }}|$PARTITION|g" \
-        -i -e "s|{{ ACCOUNT }}|$ACCOUNT_NAME|g" \
+    sed -i -e "s|{{ PARTITION }}|$WORKFLOW_PARTITION|g" \
+        -i -e "s|{{ ACCOUNT }}|$WORKFLOW_ACCOUNT|g" \
         -i -e "s|{{ SLURM_PROFILE }}|$PROFILE|g" \
-        -i -e "s|{{ PYTHON }}|$PYTHON|g" run_job.sh
+        -i -e "s|{{ PYTHON }}|$PYTHON|g" \
+        -i -e "s|{{ IMAGE_CLI }}|$IMAGE_CLI|g" run_job.sh
+
     chmod +x run_job.sh
     echo "Run Snakemake on a compute node"
-    sbatch run_job.sh
+    #sbatch run_job.sh
     # TODO we should add a flag to not clean up the sbatch script in case it's needed for debugging
 else
     echo "Run Snakemake on the login node"
     # This line might need to be tailored to your container depending how you built it
     # Right now it will only launch 1 job in parallel at a time
     # TODO the singularity args could be made configurable
-    snakemake --profile "$PROFILE" --singularity-args "--cwd /app --bind $(pwd)/output:/app/output" --jobs 1
+    snakemake --profile "$PROFILE" --jobs 1 $IMAGE_CLI
 fi
 
 if [ "$CLEANUP" == "YES" ]; then
